69bd3c4b544d3820cb46566ca8a5bbc3ae12cd4e
iinow <iinow@naver.com>
2020-05-29 22:36:35 +0900

    loader 문제 풀이

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
+const { smart } = require('webpack-merge')
 
-module.exports = {
+module.exports = smart({
   mode: 'development',
   entry: {
     main: './src/app.js'
@@ -8,5 +9,25 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'url-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+          publicPath: './dist',
+          limit: 10000
+        }
+      }
+    ]
   }
-}
+})
