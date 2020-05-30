b4a76453f2041c2b2d69bce9917976c6cdbd1219
iinow <iinow@naver.com>
2020-05-30 15:38:44 +0900

    eslint 풀이:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,18 @@
-// TODO: eslint를 구성해 보세요. 
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
